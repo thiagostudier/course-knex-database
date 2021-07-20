f380c430caddefb126c53deb8f9771530f86c8e3
Thiago dos Santos Studier <thiago.studier@hmv.org.br>
2021-07-20 17:24:00 -0300

    INSERT PERFILS

--- testes/insert.js
+++ testes/insert.js
@@ -1,11 +1,23 @@
 const db = require('../config/db');
 
-const novoPerfil = {
-    nome: 'cadastrador',
-    rotulo: 'Cadastrador'
+// const novoPerfil = {
+//     nome: 'novo',
+//     rotulo: 'novo'
+// }
+
+// db('perfis').insert(novoPerfil)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err.sqlMessage))
+//     .finally(() => db.destroy())
+
+const perfilSU = {
+    nome: 'root' + Math.random(),
+    rotulo: 'Super Usuário'
 }
 
-db('perfis').insert(novoPerfil)
+// db.insert(novoPerfil).into('perfis');
+
+db('perfis').insert(perfilSU)
     .then(res => console.log(res))
     .catch(err => console.log(err.sqlMessage))
-
+    .finally(() => db.destroy())
